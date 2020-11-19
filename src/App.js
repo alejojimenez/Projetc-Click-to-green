ac8cb481721c8811d9c723d446cac71f516004a8
Freddy Lobo <freddyloboq@gmail.com>
2020-11-19 02:35:48 -0300

    Creación del NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './App.css';
+import './styles/StyleNavbar.css';
+import NavBarTop from "./components/NavBarTop"
 
 function App() {
   return (
-    <h1>Click To Green</h1>
+    <>
+      <NavBarTop/>
+      <h1>Click To Green</h1>
+    </>
   );
 }
 
