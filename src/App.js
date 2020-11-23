f4f111e47b4fdc6f2fcf87f474030b9dea807503
Alejo Jimenez <ajimenez.rengifo@gmail.com>
2020-11-23 20:42:59 -0300

    Subiendo Vista Home con Portada

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import "./styles/custom.scss";
 import './styles/StyleNavbar.css';
 import "./styles/StylesMP.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBarTop from "./components/NavBarTop"
-import Suggestions from './views/Suggestions'
-import PayMethod from "./views/PayMethod"
+
+// SECCIONES 
+import Home from "./views/Home";
+import NavBarTop from "./components/NavBarTop";
+import Suggestions from './views/Suggestions';
+import PayMethod from "./views/PayMethod";
 
 function App() {
   return (
     <>
-      <NavBarTop />
+      <Home />
       <Suggestions />
       <BrowserRouter>
-        <NavBarTop />
+        {/* <NavBarTop /> */}
         <PayMethod />
         <div>
           <Switch>
             <Route exact path="/" component={PayMethod} />
+            <Route exact path="/" component={Suggestions} />            
             {/* <Route exact path="/Details/:name" component={Details} />
                 <Route exact path="/Details-Planets/:name" component={DetailsPlanets} /> */}
             <Route render={() => <h1>ERROR 404</h1>} />
