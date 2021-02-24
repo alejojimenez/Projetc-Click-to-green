af78ce10e81cf2a8652bb7e47c869e9ed0ac8d60
Alejo Jimenez <ajimenez.rengifo@gmail.com>
2021-02-23 23:32:49 -0300

    Integrando funcionalidad asignar notificacion v.1

--- src/components/ModalNotifications.jsx
+++ src/components/ModalNotifications.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
-// import { Context } from "../store/appContext";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Button, Modal } from "react-bootstrap";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
-const ModalNotifications = () => {
-    // const { store, actions } = useContext(Context);
+const ModalNotifications = (props) => {
+    const { store, actions } = useContext(Context);
+    const [value,setValue]=useState('');
+    const handleSelect=(e)=>{
+        console.log(e);
+        setValue(e)
+    }
 
+    useEffect(() => {
+        actions.getUsers()
+    }, [])
+
+    console.log(props);
     const [isOpen, setIsOpen] = useState(false);
 
     const showModal = () => {
@@ -20,19 +31,50 @@ const ModalNotifications = () => {
     };
 
     return (
-        <>
-            <Button className="bt bg-success" onClick={showModal}>Asignar</Button>
-            <Modal className="" show={isOpen} onHide={hideModal}>
-                <ModalHeader>
-                    <ModalTitle className="text-center colorPrincipal">Operador de Rutas</ModalTitle>
-                </ModalHeader>
-                <ModalBody>Seleccione</ModalBody>
-                <ModalFooter>
-                    <Button className="bt bg-success">Asignar Operador</Button>
-                    <Button className="bt bg-danger" onClick={hideModal}>Cancelar</Button>
-                </ModalFooter>
-            </Modal>
-        </>
+    <>
+        <Button className="bt bg-success" onClick={showModal}>
+            Asignar
+        </Button>
+        <Modal className="" show={isOpen} onHide={hideModal}>
+            <ModalHeader>
+                <ModalTitle className="text-center colorPrincipal">
+                    Operador de Rutas
+                </ModalTitle>
+            </ModalHeader>
+            <ModalBody>
+                <div className="mb-2">
+                    <DropdownButton
+                        variant=''
+                        drop='right'
+                        title="- Seleccione: "
+                        id="dropdown-menu-align-right"
+                        onSelect={handleSelect}
+                    >
+                        {console.log("Construyendo lista", store.users)}
+                        {store.users.map((item, index) => {
+                        return (
+                            <Dropdown.Item 
+                                className="colorSecundario" 
+                                key={index} 
+                                eventKey={item.username}
+                            >
+                                {item.username} {'-'} {item.rol}
+                            </Dropdown.Item>
+                            )
+                        })
+                        }
+                    </DropdownButton>
+                    <p className="pt-4 pb-2">Operador Seleccionado:  <strong>{value}</strong></p>
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button className="bt bg-success">Asignar Operador</Button>
+                <Button className="bt bg-danger" onClick={hideModal}>
+                    Cancelar
+                </Button>
+            </ModalFooter>
+        </Modal>
+    </>
     );
 };
 
